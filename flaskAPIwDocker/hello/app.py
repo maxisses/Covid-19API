52c94015c84bef0d7dad7631ada63c4817ca16ea
Maximilian Dargatz <max.dargatz@de.ibm.com>
2020-03-22 08:03:29 +0100

    added some endpoints and handled some exceptions

--- flaskAPIwDocker/hello/app.py
+++ flaskAPIwDocker/hello/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import render_template
+from datetime import datetime, timedelta
 import psycopg2
 import psycopg2.extras
 
@@ -72,10 +73,89 @@ def create_app():
         """
         return render_template("index.html")
 
-    # GET /get_total_cases
+    # GET /get_total_infected
     # (parameter = zeitraum, nation, bundesland, city, gender, agegroup ... )
-    @app.route("/get_total_cases")
-    def get_total_cases():
+    @app.route("/get_totals")
+    def get_totals():
+        state = request.args.get('state')
+        province = request.args.get('province')
+        sex = request.args.get('sex')
+        age_group_start = request.args.get('age_group_start')
+
+        try:
+            age_group_start = int(age_group_start)
+        except:
+            pass
+        age_group_end = request.args.get('age_group_end')
+
+        try:
+            age_group_end = int(age_group_end)
+        except:
+            pass
+
+        extraction_date = request.args.get('extraction_date')
+        
+        extraction_date = handle_extraction_date(extraction_date)
+
+        date_range = request.args.get('date_range')
+
+        date_range_start = None
+        date_range_end = None
+        try:
+            date_range = date_range.split(" ")
+            print(date_range)
+            if len(date_range) > 1:
+                date_range_start = date_range[0]
+                date_range_end = date_range[1]
+            else:
+                date_range_start = date_range[0]
+                date_range_end = date_range[0]
+        except:
+            pass
+
+        conn = psycopg2.connect("dbname='wirvsvirus' user='wirvsvirus' host='marc-book.de' password='[n2^3kKCyxUGgzuV'")
+        cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+
+        selection = {"extraction_date": extraction_date,"state":state, "province":province, "sex": sex, 
+                    "age_group_start":age_group_start, "age_group_end": age_group_end}
+
+        columns = []
+        for key, value in selection.items():
+            if value:
+                columns.append(key)
+        columns = ", ".join(columns)
+        print(columns)
+
+        
+        selection["date_range_start"] = date_range_start
+        selection["date_range_end"] = date_range_end
+
+        print(selection)
+        cur.execute(""" SELECT {} , sum(case_count) AS infected, sum(death_count) AS deceased
+                        FROM rki_data_germany 
+                            WHERE (extraction_date = %(extraction_date)s OR %(extraction_date)s IS NULL )
+                            AND (state = %(state)s OR %(state)s IS NULL )
+                            AND (province = %(province)s OR %(province)s IS NULL)
+                            AND (sex = %(sex)s OR %(sex)s IS NULL)
+                            AND (age_group_start >= %(age_group_start)s OR %(age_group_start)s IS NULL)
+                            AND (age_group_end <= %(age_group_end)s OR %(age_group_end)s IS NULL)
+                            AND ((notification_date BETWEEN %(date_range_start)s AND %(date_range_end)s) OR %(date_range_start)s IS NULL)
+                            GROUP BY {}""".format(columns, columns), 
+                            selection)
+
+        rows = cur.fetchall()
+
+        if len(rows) == 0:
+            return jsonify({"message": "error or no values"})
+        else:
+            if date_range_start:
+                rows[0]["from"] = date_range_start
+                rows[0]["to"] = date_range_end
+            return jsonify(rows)
+
+    # GET /get_data
+    @app.route("/get_data")
+    def get_data():
         state = request.args.get('state')
         province = request.args.get('province')
         sex = request.args.get('sex')
@@ -92,6 +172,9 @@ def create_app():
         except:
             pass
         extraction_date = request.args.get('extraction_date')
+
+        extraction_date = handle_extraction_date(extraction_date)
+
         date_range = request.args.get('date_range')
 
         date_range_start = None
@@ -118,17 +201,15 @@ def create_app():
 
         columns = []
         for key, value in selection.items():
-            if value:
-                columns.append(key)
+            columns.append(key)
         columns = ", ".join(columns)
         print(columns)
-
         
         selection["date_range_start"] = date_range_start
         selection["date_range_end"] = date_range_end
 
         print(selection)
-        cur.execute(""" SELECT {} , sum(case_count) 
+        cur.execute(""" SELECT {} ,notification_date AS reported, case_count AS infected, death_count AS deceased
                         FROM rki_data_germany 
                             WHERE (extraction_date = %(extraction_date)s OR %(extraction_date)s IS NULL )
                             AND (state = %(state)s OR %(state)s IS NULL )
@@ -137,7 +218,7 @@ def create_app():
                             AND (age_group_start >= %(age_group_start)s OR %(age_group_start)s IS NULL)
                             AND (age_group_end <= %(age_group_end)s OR %(age_group_end)s IS NULL)
                             AND ((notification_date BETWEEN %(date_range_start)s AND %(date_range_end)s) OR %(date_range_start)s IS NULL)
-                            GROUP BY {}""".format(columns, columns), 
+                            """.format(columns, columns), 
                             selection)
 
         rows = cur.fetchall()
@@ -146,39 +227,70 @@ def create_app():
             return jsonify({"message": "error or no values"})
         else:
             if date_range_start:
-                rows[0]["from"] = date_range_start
-                rows[0]["to"] = date_range_end
+                for row in rows:
+                    row["from"] = date_range_start
+                    row["to"] = date_range_end
             return jsonify(rows)
 
-    # GET /get_active_cases
-    @app.route("/get_active_cases")
-    def get_active_cases():
-        country = request.args.get('country')
-        response = []
-        my_selection = filter(lambda x: x['country'] == country, cases)
-        dict_filter= ["country", "date", "active_cases"]
-        for el in my_selection:
-            response.append({my_key: el[my_key] for my_key in dict_filter})
+    # GET /get_events
+    @app.route("/get_events")
+    def get_total_deceased():
+        state = request.args.get('state')
+        province = request.args.get('province')
+        sex = request.args.get('type')
+        age_group_start = request.args.get('description')
+        extraction_date = request.args.get('extraction_date')
+        date_range = request.args.get('date_range')
 
-        if len(response) == 0:
-            return jsonify(cases)
+        date_range_start = None
+        date_range_end = None
+        try:
+            date_range = date_range.split(" ")
+            print(date_range)
+            if len(date_range) > 1:
+                date_range_start = date_range[0]
+                date_range_end = date_range[1]
+            else:
+                date_range_start = date_range[0]
+                date_range_end = date_range[0]
+        except:
+            pass
+
+        return None
+
+    # GET /get_diff_from_to
+    @app.route("/get_diff_from_to")
+    def get_get_diff_from_to():
+        state = request.args.get('state')
+        province = request.args.get('province')
+        sex = request.args.get('type')
+        age_group_start = request.args.get('description')
+        extraction_date = request.args.get('extraction_date')
+        date_range = request.args.get('date_range')
+
+        date_range_start = None
+        date_range_end = None
+        try:
+            date_range = date_range.split(" ")
+            print(date_range)
+            if len(date_range) > 1:
+                date_range_start = date_range[0]
+                date_range_end = date_range[1]
+            else:
+                date_range_start = date_range[0]
+                date_range_end = date_range[0]
+        except:
+            pass
+
+        return None
+
+    def handle_extraction_date(date):
+        if date:
+            return date
         else:
-            return jsonify(response)
-
-    # GET /get_died_cases
-    @app.route("/get_died_cases")
-    def get_died_cases():
-        country = request.args.get('country')
-
-        response = []
-        my_selection = filter(lambda x: x['country'] == country, cases)
-        dict_filter= ["country", "date", "died_cases"]
-        for el in my_selection:
-            response.append({my_key: el[my_key] for my_key in dict_filter})
-
-        if len(response) == 0:
-            return jsonify(cases)
-        else:
-            return jsonify(response)
-
+            #extraction_date = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+            date = "2020-03-21"
+            print("set extraction date to yesterday: " + str(date))
+            return date
+    
     return app
